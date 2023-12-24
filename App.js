3ed3d39a24e63686f98376c20f2a3f4a5d020abc
aircongo <natelufuluabo@yahoo.ca>
2023-12-24 11:33:36 -0500

    finsihed building the home page

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import imageSource from './assets/homscreen.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={imageSource} style={styles.image} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
+          Stay connected with your loved ones
+          & chat with them anytime, anywhere!
+        </Text>
+      </View>
+      <View style={StyleSheet.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +26,44 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    height: '100%',
+    paddingHorizontal: 20
+  },
+  image: {
+    height: 450,
+    width: 370
+  },
+  textContainer: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#6C63FF'
+  },
+  button: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#6C63FF',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 30
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    letterSpacing: 1.5
+  },
+  buttonContainer: {},
 });
