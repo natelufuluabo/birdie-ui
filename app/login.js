aa2aa14c0ff069b41d6c26edc542175edd57beca
aircongo <natelufuluabo@yahoo.ca>
2023-12-24 11:51:44 -0500

    created login page

---
+++ app/login.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+
+export default function Login() {
+    retun (
+        <View>
+            Login page
+            <StatusBar style="auto" />
+        </View>
+    );
+}
