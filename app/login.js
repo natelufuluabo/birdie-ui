c3256fe7eab27522694aaf481cb2068c06e407a5
aircongo <natelufuluabo@yahoo.ca>
2023-12-25 10:59:23 -0500

    added image for login page

--- app/login.js
+++ app/login.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text, Image, SafeAreaView, Pressable } from "react-native";
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Login() {
     return (
