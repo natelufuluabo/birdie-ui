47c7b3ff6da5d1da73aab338faec298c43b82dc2
aircongo <natelufuluabo@yahoo.ca>
2023-12-25 10:43:06 -0500

    created login page

---
+++ app/login.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text, Image, SafeAreaView, Pressable } from "react-native";
+
+export default function Login() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Login Page</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
