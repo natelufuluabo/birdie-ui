e0c69f03e74baad36c4749acd45bba3fa1dd65df
aircongo <natelufuluabo@yahoo.ca>
2023-12-27 08:36:20 -0500

    edited signups utils file so that it uses the utils in the shared file

--- utils/signups.js
+++ utils/signups.js
@@ -1,17 +1,4 @@
-const validateEmail = (text) => {
-    if (text === '') return false;
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (emailRegex.test(text)) return true;
-    return false;
-};
-
-const validatePassword = (text) => {
-    // Password must be at least 8 characters, max 14 characters, with a mix of uppercase and lowercase letters, and at least 1 number
-    if (text === '') return false;
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,14}$/;
-    if (passwordRegex.test(text)) return true;
-    return false;
-};
+import { validateEmail, validatePassword } from "./shared";
 
 export const validateForm = (formData, setErrorsObject) => {
     const { username, email, password } = formData;
