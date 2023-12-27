b0ac6aea947b6667aa7551e439614516ed601a9f
aircongo <natelufuluabo@yahoo.ca>
2023-12-27 08:38:30 -0500

    logins utils to validate form

---
+++ utils/logins.js
@@ -0,0 +1,21 @@
+import { validateEmail, validatePassword } from "./shared";
+
+export const validateForm = (formData, setErrorsObject) => {
+    const { username, email, password } = formData;
+    if (!validateEmail(email)) {
+        setErrorsObject(prevState => ({ 
+            ...prevState, usernameError: '',
+            emailError: 'Invalid Email',
+            passwordError: ''
+        }));
+        return false
+    }
+    if (!validatePassword(password)) {
+        setErrorsObject(prevState => ({ 
+            ...prevState, passwordError: 'Invalid password. Password must: \n - Be at least 8 and at most 14 characters. \n - Contain a mix of uppercase and lowercase letters. \n - Contain at least one digit',
+            usernameError: '', emailError: ''
+        }));
+        return false
+    }
+    return true;
+}
