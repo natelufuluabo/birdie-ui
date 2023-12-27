cda73a126c4fd364396229851c0dbd2f6f3f5325
aircongo <natelufuluabo@yahoo.ca>
2023-12-27 07:31:47 -0500

    created confirmation email component

---
+++ components/confirmation_email.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text, Image, SafeAreaView, Pressable } from "react-native";
+import { Link } from 'expo-router';
+
+export default function ConfirmationEmail({ setSignUpSuccessfull }) {
+    return (
+        <View>
+            <Text>ConfirmationEmail</Text>
+        </View>
+    );
+}
