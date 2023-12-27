aa1b1f4c0052e91a49f6915e5553c7dad518f531
aircongo <natelufuluabo@yahoo.ca>
2023-12-27 07:58:23 -0500

    finished building confirmation ui

--- app/signup.js
+++ app/signup.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, Image, SafeAreaView, Pressable } from "react-native";
-import { Link } from 'expo-router';
+import { StyleSheet, SafeAreaView } from "react-native";
 import SignUp from '../components/signup_main';
+import ConfirmationEmail from '../components/confirmation_email';
 
 
 export default function Signup() {
+    const [signUpSuccessfull, setSignUpSuccessfull] = useState(true);
     return (
         <SafeAreaView>
-            <SignUp />
+            { signUpSuccessfull ? <ConfirmationEmail setSignUpSuccessfull={setSignUpSuccessfull} /> : <SignUp /> }
             <StatusBar style="auto" />
         </SafeAreaView>
     );
--- components/confirmation_email.js
+++ components/confirmation_email.js
@@ -1,12 +1,74 @@
 import React, { useState } from 'react';
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, Image, SafeAreaView, Pressable } from "react-native";
+import { StyleSheet, View, Text, Image,Pressable } from "react-native";
 import { Link } from 'expo-router';
+import imageSource from '../assets/successImg.png';
 
 export default function ConfirmationEmail({ setSignUpSuccessfull }) {
     return (
-        <View>
-            <Text>ConfirmationEmail</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image source={imageSource} style={styles.image} />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>Registration successful!</Text>
+                <Text style={styles.paragraph}>We're thrilled to welcome you to our messaging community! Thank you for creating an account and initiating your journey toward connecting and communicating through our app.</Text>
+            </View>
+            <Pressable style={styles.button} onPress={() => setSignUpSuccessfull(false)}>
+                <Link href='/login' asChild>
+                    <Text style={styles.buttonText}>Continue</Text>
+                </Link>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100%',
+        gap: 20,
+        paddingHorizontal: 20,
+        paddingTop: 40
+    },
+    image: {
+        height: 400,
+        width: 400
+    },
+    textContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 15,
+        justifyContent: 'center',
+        width: '100%'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#6C63FF'
+    },
+    paragraph: {
+        textAlign: 'center',
+        lineHeight: 20,
+        fontSize: 14
+    },
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#6C63FF',
+        paddingVertical: 15,
+        paddingHorizontal: 50,
+        borderRadius: 20,
+        width: '100%',
+        alignSelf: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold',
+        letterSpacing: 1.5
+    },
+})
