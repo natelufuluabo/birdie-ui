e4540b1df836ae214f14a8a6f85ded87c50b7cf9
aircongo <natelufuluabo@yahoo.ca>
2024-01-04 02:44:43 -0500

    implemented navigation from profile page to contact us page

--- screens/profile.js
+++ screens/profile.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfileHome from './profile_home';
 import ProfileDetails from './profile_details';
 import ProfileSettings from './profile_settings';
+import ProfileContactUs from './profile_contactus';
 
 export default function Profile() {
     const Stack = createNativeStackNavigator();
@@ -10,6 +11,7 @@ export default function Profile() {
             <Stack.Screen name='Home' component={ProfileHome} />
             <Stack.Screen name='details' component={ProfileDetails} />
             <Stack.Screen name='settings' component={ProfileSettings} />
+            <Stack.Screen name='contactus' component={ProfileContactUs} />
         </Stack.Navigator>
     );
 }
---
+++ screens/profile_contactus.js
@@ -0,0 +1,7 @@
+import CustomHeader from '../components/CustomHeader';
+
+export default function ProfileContactUs() {
+    return (
+        <CustomHeader title="Contact Us" />
+    );
+}
