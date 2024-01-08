dc1ce6b176a67bb49914cc2a8def1381b20e5e10
aircongo <natelufuluabo@yahoo.ca>
2024-01-07 19:21:22 -0500

    built one section of the settings page

--- screens/profile_settings.js
+++ screens/profile_settings.js
@@ -1,7 +1,80 @@
+import React, { useState } from 'react';
 import CustomHeader from '../components/CustomHeader';
+import { View, Text, Pressable, Switch, StyleSheet } from 'react-native'; 
 
 export default function ProfileSettings() {
+    const [isEnabled, setIsEnabled] = useState(false);
+
+    const toggleSwitch = () => {
+        setIsEnabled(previousState => !previousState);
+    };
     return (
-        <CustomHeader title="Settings" />
+        <View style={styles.container}>
+            <CustomHeader title="Settings" />
+            <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>GENERAL</Text>
+                <View style={styles.sectionSection}>
+                    <Text style={styles.sectionTextContainer}>Allow Push Notifications</Text>
+                    <Switch
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={toggleSwitch}
+                        value={isEnabled}
+                    />
+                </View>
+                <View style={styles.sectionSection}>
+                    <Text style={styles.sectionTextContainer}>Enable Face ID/Touch ID</Text>
+                    <Switch
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={toggleSwitch}
+                        value={isEnabled}
+                    />
+                </View>
+            </View>
+            <Pressable style={styles.saveButton}>
+                <Text style={styles.saveText}>Save</Text>
+            </Pressable>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 30
+    },
+    sectionContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    sectionTitle: {
+        color: '#7D7C7C',
+        marginLeft: 15,
+        marginBottom: 10,
+        fontSize: 16
+    },
+    sectionSection: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 7,
+        paddingHorizontal: 15,
+        backgroundColor: '#fff',
+        borderTopWidth: .5,
+        borderBottomWidth: .5
+    },
+    sectionTextContainer: {
+        fontSize: 16
+    },
+    saveText: {
+        fontSize: 18,
+        color: '#6C63FF',
+        fontWeight: 'bold'
+    },
+    saveButton: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
