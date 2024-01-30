78ff9f82584a5e8d240e86f6512efbd19beb3ca4
aircongo <natelufuluabo@yahoo.ca>
2024-01-29 19:59:13 -0500

    created the folder that will contain the friends feature code

---
+++ screens/protectedScreens/friends/friends.js
@@ -0,0 +1,9 @@
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
+
+export default function Profile() {
+    return (
+        <View>
+            <Text>Friends Tab</Text>
+        </View>
+    );
+}
